afc9d88424d3647dcd5309d2a3012e978073b053
Abdirisak-67 <abdirisaqm11@gmail.com>
2025-06-25 00:09:43 +0300

    Update useCalledStudents.js

--- src/pages/useCalledStudents.js
+++ src/pages/useCalledStudents.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { FaCheckCircle } from 'react-icons/fa';
+import { ApiBaseUrl } from '../Context/Context';
 
 const useCalledStudents = () => {
   const [students, setStudents] = useState([]);
+  const apiBaseUrl = useContext(ApiBaseUrl);
   useEffect(() => {
-    axios.get('http://localhost:5000/api/students')
+    axios.get(`${apiBaseUrl}/api/students`)
       .then(res => setStudents(res.data.filter(s => s.called)))
       .catch(() => setStudents([]));
-  }, []);
+  }, [apiBaseUrl]);
   return students;
 };
 
